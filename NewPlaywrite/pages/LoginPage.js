335b8abc59ada19d5050b612b435645511677678
DiwasDhital <todiwasdhital@gmail.com>
2026-02-18 22:04:08 +0545

    updated comments

--- NewPlaywrite/pages/LoginPage.js
+++ NewPlaywrite/pages/LoginPage.js
@@ -2,7 +2,7 @@
 const { expect } = require('@playwright/test');
 
 exports.LoginPage = class LoginPage {
-
+ 
   constructor(page) {
     this.page = page;
     
--- NewPlaywrite/tests/login.spec.js
+++ NewPlaywrite/tests/login.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/LoginPage'); // Import your remote control
 
 test('Simple Login Test', async ({ page }) => {
-  // test.setTimeout(60000);
+  test.setTimeout(100000);
   // 1. Switch on the remote
   const loginPage = new LoginPage(page);
 
@@ -12,7 +12,7 @@ test('Simple Login Test', async ({ page }) => {
 
   // 3. Do the login (pass the email and password here)
   await loginPage.doLogin('diwas@gmail.com', 'dfaasdfsdaf');
-
   // 4. Check if the error appeared
   await expect(loginPage.errorMsg).toBeVisible();
 });
+
